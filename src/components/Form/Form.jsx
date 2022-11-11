0d93fe8c1079cf70d8a4d03957d633c4a91526ca
ivandjuka77 <captainsubzer@gmail.com>
2022-11-11 17:10:24 +0100

    Changed form styling.

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -8,7 +8,7 @@ const Form = () => {
 
 	return (
 		<form>
-			<label for='url'>URL or Text:</label>
+			<label htmlFor='url'>URL or Text:</label>
 			<input
 				type='text'
 				placeholder='Enter Your URL/Text'
@@ -16,19 +16,24 @@ const Form = () => {
 				value={url}
 				onChange={(e) => setUrl(e.target.value)}
 			/>
-			<br />
-			<label for='dimensions'>Size</label>
+			<label htmlFor='dimensions'>Size</label>
 			<select
 				id='dimensions'
 				onChange={(e) => setDimensions(e.target.value)}
 			>
 				<option value='100'>100x100</option>
+				<option value='128'>128x128</option>
 				<option value='200'>200x200</option>
+				<option
+					value='256'
+					selected
+				>
+					256x256
+				</option>
 				<option value='300'>300x300</option>
 				<option value='400'>400x400</option>
-				<option value='500'>500x500</option>
-				<option value='600'>600x600</option>
 			</select>
+			<button>Generate</button>
 		</form>
 	);
 };
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
-import Form from '../Form/Form';
+
 import QRCode from 'react-qr-code';
+
+import Form from '../Form/Form';
 
 import './Main.css';
 
@@ -8,19 +10,24 @@ const Main = () => {
 	return (
 		<div className='main'>
 			<div className='main-inner'>
-				<div className='main-left'>
-					<div className='main-text'>
-						<h1>Generate your QR Code in seconds!</h1>
-						<p>
-							1. Put your URL/Text and the size below <br />
-							2. Viola! Your QR Code will appear on the left. <br />
-							3. You can download it by clicking the button below the QR Code
-						</p>
-					</div>
-
+				<div className='main-form-text'>
+					<h1>Generate your QR Code in seconds!</h1>
+					<p>
+						1. Put your URL/Text and the size below <br />
+						2. Viola! Your QR Code will appear on the left. <br />
+						3. You can download it by clicking the button below the QR Code
+					</p>
+					<br />
 					<Form />
 				</div>
-				<QRCode value='' />
+				<div className='main-qr-div'>
+					<QRCode
+						value='https://www.youtube.com/watch?v=qNiUlml9MDk&t=727s&ab_channel=TraversyMedia'
+						className='main-qr'
+						size={256}
+					/>
+					<button className='main-qr-btn'>Download QR Code</button>
+				</div>
 			</div>
 		</div>
 	);
